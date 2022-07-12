bba67b713137220fda949690c911305be5652355
Turu0512 <kosuke.tsurugano@garage-ventures.com>
2022-07-13 08:49:06 +0900

    後の実装は出来るため終了

--- react-graphql-udemy/src/App.js
+++ react-graphql-udemy/src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from "react";
 import { ApolloProvider } from "react-apollo";
 import { Query } from "react-apollo";
 import client from "./client";
-import { ME } from "./graphql";
+import { SEARCH_REPOSITORIES } from "./graphql";
+
+const VALIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア",
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = VALIABLES;
+  }
   render() {
     return (
       <ApolloProvider client={client}>
-        <div>hello</div>
-
         <Query query={ME}>
           {({ loading, error, data }) => {
             if (loading) return "loading...";
